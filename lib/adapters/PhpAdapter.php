458e4de317253dde268cb276daebf88b7c7620fd
Ross <ross@oneblackbear.com>
2010-10-31 09:13:17 +0000

    Phrocco Source import

---
+++ lib/adapters/PhpAdapter.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * PHP Adapter Class
+ * Its job is to handle php code and create an array of
+ * code and comments from a php code file.
+ * @author Ross Riley
+ **/
+
+class PhpAdapter {
+  // Regular Expression to find strip out all type of comments characters
+  public $comment_blocks = array(
+    "#/\*+\s#ms",             // Multi Start
+    "#\*+/#ms",               // Multi Block End
+    "#/\*+([^\n]*)\*+/#ms",   // Multi Block on one line
+    "#\s*\*([^\n]*\n)#ms",      // Multi Block
+    "#//([^\n]*)#ms"          // Single Comment Line
+  );
+
+  
+  // Looks for phpdoc parameters eg: @author, @param, @return
+  public $doc_params    = "#\s(@[^\s]*)#"; 
+  
+  public $comment_tokens = array("T_COMMENT", "T_DOC_COMMENT");
+  
+  /**
+   * Main parsing method.
+   * Uses the php tokenizer to separate comments from code
+   * @return `array` 
+   *    array("code"=>`array of code lines`, "docs"=>`array of doc blocks`)
+   *
+   **/
+  public function parse($file) {
+    $fname = $file;
+    $file = file_get_contents($file);
+    $code = "";
+    $all_matches = array();
+    foreach(token_get_all($file) as $tok) {
+      if(is_array($tok)) {
+        if(in_array(token_name($tok[0]), $this->comment_tokens)) {
+          $tok[1] = str_replace("\t","  ",$tok[1]);
+          $all_matches[]=preg_replace($this->comment_blocks,"$1", $tok[1]);
+          $code.="\n//CODEBLOCK\n";
+        } else {
+          $code.=$tok[1];
+        }                  
+      } else $code.=$tok;     
+    }
+
+    // Passes code onto pygmentize to add syntax highlighting
+    $pyg = new Pygment;
+    $code = $pyg->pygmentize("php",$code);
+    $code = explode('<span class="c1">//CODEBLOCK</span>', $code);
+    array_shift($code);
+    foreach($code as &$val) {
+      $val = rtrim($val);
+      $val = str_replace("\t", " ", $val);
+      $val='<div class="highlight"><pre>'.trim($val, "\n\r").'</pre></div>'; 
+    }
+    
+    // Adds html markup to identify php docblock parameters
+    foreach($all_matches as $match) {
+      $match = preg_replace($this->doc_params, "<em class='docparam'>$1</em>", $match);
+      $docs[] = Markdown($match);
+    }
+    // Our final array of code mapped to comments
+    return array("code"=>$code,"docs"=>$docs);
+  }
+  
+}
---
+++ phrocco.php
@@ -0,0 +1,105 @@
+#!/bin/sh
+PHP=`which php`
+exec $PHP -C -q -d output_buffering=1 "$0" "$@"
+<?php
+require_once("lib/markdown.php");
+require_once("lib/pygment.php");
+
+
+class Phrocco {
+
+  public $sources;
+  public $sections = array();
+  public $title;
+  public $file;
+  public $adapter;
+  public $output_file;
+  public $path;
+
+  
+  public function __construct($language, $file) {
+    $this->adapter = ucfirst($language)."Adapter";
+    require_once(__DIR__."/lib/adapters/".$this->adapter.".php");
+    $this->adapter = new $this->adapter;
+    $this->file = $file;
+    $this->title = basename($this->file);
+  }
+  
+  public function parse() {
+    $this->sections = $this->adapter->parse($this->file);
+  }
+  
+
+
+  public function render() {
+    ob_start();
+    $this->parse();
+    $view_file = __DIR__."/lib/template/layout.html";
+    $this->style = file_get_contents(__DIR__."/lib/template/layout.css");
+    extract((array)$this);
+  	if(!is_readable($view_file)) throw new Exception("Unable to find Template File");
+  	if(!include($view_file)) throw new Exception("PHP Error in $view_file");
+  	$content = ob_get_contents();
+		ob_end_clean();
+		file_put_contents($this->output_file, $content);    
+  }
+
+
+}
+
+class PhroccoIterator extends RecursiveDirectoryIterator {
+    public function getExtension() {
+        $Filename = $this->getFilename();
+        $FileExtension = strrpos($Filename, ".", 1) + 1;
+        if ($FileExtension != false)
+            return strtolower(substr($Filename, $FileExtension, strlen($Filename) - $FileExtension));
+        else
+            return "";
+    }
+}
+
+class PhroccoGroup {
+  
+  public $extensions = array(
+    "php" => array("php","phps","phpt")
+  );
+  public $language = "php";
+  public $defaults = array(
+    "i"   => __DIR__,
+    "l"   => "php",
+    "o"   => false
+  );
+  public $options = array();
+  public $group = array();
+  
+  public function __construct($options) {
+    $sources = array();
+    $this->options = $options + $this->defaults;
+    $dir_iterator = new PhroccoIterator($this->options["i"]);
+    $iterator = new RecursiveIteratorIterator($dir_iterator, RecursiveIteratorIterator::SELF_FIRST);
+    foreach ($iterator as $file) {
+      if(!$iterator->isDot() && in_array($iterator->getExtension(), $this->extensions[$this->options["l"]])) {
+        $base_path = $this->options["i"];
+        $rpath = str_replace($base_path, "",$file->getPath());
+        $phrocco = new Phrocco($this->options["l"], $file);
+        if(!$this->options["o"]) $output_dir = $file->getPath();
+        else $output_dir = $this->options["o"];
+        if($rpath !=$file->getPath()) $output_dir.="/".$rpath;
+        if(!is_writable($output_dir)) @mkdir($output_dir, 0777, true);
+        if(!is_writable($output_dir)) throw new Exception("Invalid Output Directory - Couldn't Create Because of Permissions");
+        $file_out = $output_dir."/".$file->getBasename($iterator->getExtension())."html";
+        $phrocco->output_file = $file_out;
+        $phrocco->path = "./".$iterator->getSubPath();
+        $this->group[$file->getBasename()] = $phrocco;
+        $this->sources[] = array("url"=>$iterator->getSubPath()."/".$file->getBasename($iterator->getExtension())."html", "name"=>$file->getBasename());
+      }
+    }
+    foreach($this->group as $name=>$file) {
+      $file->sources = $this->sources;
+      echo "*** Processing: ".$name."\n";
+      
+      $file->render();
+    }
+    
+  }
+}
